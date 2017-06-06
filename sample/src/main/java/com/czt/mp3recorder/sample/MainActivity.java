df4dd2e27cc9265b337e6e5be7f9a50e8071afd5
zbf <516845590@qq.com>
2017-06-06 16:57:03 +0800

    更改一下gradle/maven版本

--- sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
+++ sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
@@ -13,37 +13,47 @@ import android.widget.Button;
 
 import com.czt.mp3recorder.MP3Recorder;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-	private MP3Recorder mRecorder = new MP3Recorder(new File(Environment.getExternalStorageDirectory(),"test.mp3"));
+    private MP3Recorder mRecorder = new MP3Recorder(new File(Environment.getExternalStorageDirectory(), "test.mp3"));
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Button startButton = (Button) findViewById(R.id.StartButton);
-		startButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					mRecorder.start();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		Button stopButton = (Button) findViewById(R.id.StopButton);
-		stopButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mRecorder.stop();
-			}
-		});
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button startButton = (Button) findViewById(R.id.StartButton);
+        startButton.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    mRecorder.start();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button stopButton = (Button) findViewById(R.id.StopButton);
+        stopButton.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mRecorder.stop();
+            }
+        });
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		mRecorder.stop();
-	}
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mRecorder.stop();
+    }
 }
